92dd5663b59d97b9cdff689a70078c9b9de2732e
Datojanezashvili <datojanezashvili9@gmail.com>
2025-05-13 20:50:41 +0400

    Website Loader - Demo

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('main').style.display = 'block';
+    }, 2000); // 5 seconds delay
+  });
+  
